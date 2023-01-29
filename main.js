98e415f965cffeacf53cad6382ec0930b7418d1c
santiago s <santisica29@gmail.com>
2023-01-29 14:07:01 -0300

    fixed bugs for when there was no input and no 'get quote' button was pressed

--- main.js
+++ main.js
@@ -23,6 +23,7 @@ function fetchQuote() {
 function getAnswer() {
   const placeForAnswer = document.querySelector('h3')
   placeForAnswer.innerText = `- ${answer}`
+  const placeForQuote = document.querySelector('h2');
 
   // turn input value to uppercase
   const inputVal = document.querySelector('input').value.toLowerCase()
@@ -31,16 +32,16 @@ function getAnswer() {
     arr[i] = arr[i].charAt(0).toUpperCase() + arr[i].slice(1);
   }
   const inputVal2 = arr.join(" ");
- 
-
 
   const result = document.querySelector('h4');
 
-  if (inputVal2 === answer) {
+  if (inputVal2 === '') {
+    return ''
+  } else if (answer === '') {
+    result.innerText = 'Press the \'Get Quote\' to get started'
+  }else if (inputVal2 === answer) {
     result.style.color = 'green'
     result.innerText =  `Correct! ${inputVal2} did said that`
-  } else if(inputVal2 === '') {
-
   } else {
     result.style.color = 'red'
     result.innerText =  `Wrong! ${inputVal2} did not said that.\n It was ${answer} who said it!`
